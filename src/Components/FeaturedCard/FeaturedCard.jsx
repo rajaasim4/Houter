b12d1a1b16981303f3c9a7cbaba0b3e3d7e14943
Raja Asim <98418439+rajaasim4@users.noreply.github.com>
2022-12-30 18:11:55 +0500

    Card Issues fixed

--- src/Components/FeaturedCard/FeaturedCard.jsx
+++ src/Components/FeaturedCard/FeaturedCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FeaturedHouseCard = (props) => {
   return (
-    <div className="card rounded-5 my-2 mx-auto" style={{ width: "20rem" }}>
+    <div className="card rounded-5 my-2 mx-auto" style={{ width: "19rem" }}>
       <img src={props.src} className="card-img-top w-100 h-75" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
